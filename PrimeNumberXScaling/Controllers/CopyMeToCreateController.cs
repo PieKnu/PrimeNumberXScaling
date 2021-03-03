538f3c0365ba4757ff0da43070e66208847770cc
Pierre Hübertz Knudsen <pierreknudsen123@gmail.com>
2021-03-03 12:41:55 +0100

    Add project files.

---
+++ PrimeNumberXScaling/Controllers/CopyMeToCreateController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeNumberXScaling.Controllers
+{
+    [ApiController]
+    public class CopyMeToCreateController : ControllerBase
+    {
+
+        public CopyMeToCreateController()
+        {
+
+        }
+
+
+        [HttpPost]
+        [Route("api/ControllerNameOrDifferent/PostName")]  // use "/{id:int}/" for int & or {someStringName} for string in / as additional URL parameter
+        public IActionResult PostName([FromBody] RegistrationModel model) // IF we/u use additional url parameter, u have to add/parse it here as argument/parameter eks: ([FromBody] RegistrationModel model, int id, string someStringName)
+        {
+
+        }
+    }
+}
